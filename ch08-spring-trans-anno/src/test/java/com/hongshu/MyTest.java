65bb1420562f09dd0a9f7911e02dbeb08e3ade0f
HongShu995 <759631477@qq.com>
2021-11-11 20:06:37 +0800

    第四天

---
+++ ch08-spring-trans-anno/src/test/java/com/hongshu/MyTest.java
@@ -0,0 +1,21 @@
+package com.hongshu;
+
+import com.hongshu.service.BuyGoodsService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest
+{
+    @Test
+    public void Test01()
+    {
+        String config = "applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        BuyGoodsService service = (BuyGoodsService) ac.getBean("buyGoodsService");
+        //service是代理：com.sun.proxy.$Proxy16:jdk动态代理对象
+        System.out.println("service是代理："+service.getClass().getName());
+        service.buy(1001,10);
+
+    }
+}
---
+++ ch09-spring-trans-anno-aspectj/src/main/java/com/hongshu/dao/SaleDao.java
@@ -0,0 +1,9 @@
+package com.hongshu.dao;
+
+import com.hongshu.entity.Sale;
+
+public interface SaleDao
+{
+    //增加销售记录
+    int insertSale(Sale sale);
+}
